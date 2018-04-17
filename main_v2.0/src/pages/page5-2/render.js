297062f79eea1ddb5d5cce557d2ca65c4484157d
zhangbc <zhangbc@mail.taiji.com.cn>
2018-04-17 17:29:14 +0800

    v2.0

---
+++ main_v2.0/src/pages/page5-2/render.js
@@ -0,0 +1,22 @@
+﻿
+import React, { Component } from 'react';
+
+class Page extends Component {
+
+
+
+
+
+	render() {
+
+		return (
+			<div>
+				Page5_2页面
+			</div>
+			)
+
+	}
+
+}
+
+export { Page };
